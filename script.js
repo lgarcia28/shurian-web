96c942acfe01b960c6b4dc78704e5c5f60af47f8
lgarcia28 <leoneldariogarcia@gmail.com>
2025-11-02 01:53:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const navbar = document.querySelector('.navbar');
 
+    // Evento para mostrar/ocultar el menú al hacer clic en el ícono
     hamburgerMenu.addEventListener('click', () => {
         navbar.classList.toggle('active');
     });
 
-    // Cierra el menú si se hace clic en un enlace (para móviles)
+    // Opcional: Cierra el menú si se hace clic en un enlace
     const navLinks = document.querySelectorAll('.navbar a');
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
